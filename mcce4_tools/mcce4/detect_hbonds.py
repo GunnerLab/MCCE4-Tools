0be40bc69d50babd0e7f39f718ed3ded5e2aa9d3
Cat Chenal <catchenal@gmail.com>
2025-11-25 18:10:54 -0500

    Closes #68

--- mcce4_tools/mcce4/detect_hbonds.py
+++ mcce4_tools/mcce4/detect_hbonds.py
@@ -5,18 +5,22 @@ Module: detect_hbonds.py
 
 Description:
 This module detects hydrogen bonds between conformers atoms in the input mcce pdb file, 
-step2_out.pdb by default, and outputs their list to file <pdb folder>/hah.txt.
-A H-bond-blocking atoms list is also output to file <pdb folder>/blocking.txt if any
-are found.
-The input pdb file is first parsed for coordinate lines, which can include or exclude
-backbone atoms as per users' choice passed with the --include_bk flag; they are excluded
+step2_out.pdb by default, and outputs their list to file <pdb>_hah.txt.
+A H-bond-blocking atoms list is also output to file <pdb>_blocking.txt if any
+are found, or an empty file if no_empty_files is False (default).
+
+The input pdb file is first parsed for coordinate lines, which can exclude or include
+backbone atoms as per users' choice passed with the --no_bk flag; they are included
 by default.
 This script is self-contained, with numpy as only dependency and can be run independently
 of the MCCE4 codebase.
 
-Compared to step 6 hah.txt output:
+Comparing step6.py and detect_hbonds calculations:
   Step 6 excludes hydrogen bonds with the backbone atoms; here it's a choice.
   Step 6 doesn't check if the potential hydrogen bonds are blocked by 3rd atom.
+
+Note:
+  The blocking.txt file shows what step6 does not do; the main file is _hah.txt.
 """
 import argparse
 from collections import defaultdict
@@ -31,13 +35,17 @@ import numpy as np
 H_REGEX = re.compile(r"(^[0-9]|^[a-z])?H")
 
 
-# The H-bond distance parameters are roughly based on https://ctlee.github.io/BioChemCoRe-2018/h-bond/
+# The H-bond distance parameters are roughly based on 
+# https://ctlee.github.io/BioChemCoRe-2018/h-bond/
 # except for ANGCUT = 100 (90 here).
 # To match old program, we use the following parameters
 DNEAR = 2.0     # Min Distance cutoff for hydrogen bond between heavy atoms
 DFAR  = 4.0     # Max Distance cutoff for hydrogen bond between heavy atoms
-ANGCUT = 90     # Angle cutoff for hydrogen bond between heavy atoms, 180 is ideal, 100 is the smallest angle allowed
-ANGBLOCK = 90   # Angle cutoff for blocking atoms, 180 is ideal, 90 is the smallest angle allowed for not blocking
+ANGCUT = 90     # Angle cutoff for hydrogen bond between heavy atoms, 180 is
+                # ideal, 100 is the smallest angle allowed
+ANGBLOCK = 90   # Angle cutoff for blocking atoms, 180 is ideal, 90 is the 
+                # smallest angle allowed for not blocking
+
 # Minimum charge for hydrogen bond donor/acceptor, my best guess
 MIN_NCRG = -0.2 # Minimum heavy atom charge for hydrogen bond donor/acceptor
 MIN_HCRG = 0.2  # Minimum H atom charge for hydrogen bond donor/acceptor
@@ -45,7 +53,7 @@ CRITERIA = f"""
 Criteria:
   Donor-Acceptor (heavy atoms) Distance                : {DNEAR:1.1f} - {DFAR:1.1f}
   D-H-A Angle (Angle >= this to qualify H bond)        : {ANGCUT:3.0f}
-  H--A-? Blocking Angle (Angle <= this to block H bond): {ANGBLOCK:3.0f}
+  H--A-? Blocking Angle (Angle <= this blocks H bond)  : {ANGBLOCK:3.0f}
   Heavy Atom Charge for H bond Donor/Acceptor          : < {MIN_NCRG:4.1f}
   H Atom Charge for H bond Donor                       : > {MIN_HCRG:3.1f}
 """
@@ -71,21 +79,25 @@ class Atom:
         self.conn12 = []
 
     def loadline(self, line):
-        self.atom_name = line[12:16]
-        self.res_name = line[17:20]
+        # fail fast, do conversions first
         try:
             self.res_seq = int(line[22:26])
-        except ValueError:
-            print("The input pdb is not in mcce format (as in step2_out.pdb)")
-            sys.exit(1)
+            self.xyz = (float(line[30:38]), float(line[38:46]), float(line[46:54]))
+            self.charge = float(line[62:74])
+            self.confNum = int(line[27:30])
+        except ValueError as e:
+            sys.exit(f"Could not convert some fields from pdb line: {line!s}; Error: {e}")
 
+        self.atom_name = line[12:16]
+        self.res_name = line[17:20]
         self.chain_id = line[21]
         self.iCode = line[26]
-        self.xyz = (float(line[30:38]), float(line[38:46]), float(line[46:54]))
-        self.charge = float(line[62:74])
-        self.confNum = int(line[27:30])
         self.confType = "%3s%2s" % (self.res_name, line[80:82])
-        self.confID = "%5s%c%04d%c%03d" % (self.confType, self.chain_id, self.res_seq, self.iCode, self.confNum)
+        self.confID = "%5s%c%04d%c%03d" % (self.confType,
+                                           self.chain_id,
+                                           self.res_seq,
+                                           self.iCode,
+                                           self.confNum)
 
     def is_H(self) -> bool:
         """Check if an atom name corresponds to a hydrogen atom."""
@@ -101,21 +113,27 @@ class Atom:
                           self.confNum, self.confType, self.confID)
 
 
-def dist(atom1, atom2):
+def dist(atom1: Atom, atom2: Atom):
     """Return the distance between two atoms."""
-    return ((atom1.xyz[0] - atom2.xyz[0])**2 + (atom1.xyz[1] - atom2.xyz[1])**2 + (atom1.xyz[2] - atom2.xyz[2])**2)**0.5
+    return ((atom1.xyz[0] - atom2.xyz[0])**2 
+            + (atom1.xyz[1] - atom2.xyz[1])**2 
+            + (atom1.xyz[2] - atom2.xyz[2])**2)**0.5
 
 
-def vec(atom1, atom2):
+def vec(atom1: Atom, atom2: Atom):
     """Return the vector from atom1 to atom2."""
     return (atom2.xyz[0] - atom1.xyz[0],
             atom2.xyz[1] - atom1.xyz[1],
             atom2.xyz[2] - atom1.xyz[2])
 
 
-def deg_angle(v1, v2):
+def deg_angle(v1, v2) -> float:
     """Return the angle between two vectors in degrees."""
-    return np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))) * 180 / np.pi
+    norm1 = np.linalg.norm(v1)
+    norm2 = np.linalg.norm(v2)
+    if norm1 == 0 or norm2 == 0:
+        return 0.0
+    return np.degrees(np.arccos(np.dot(v1, v2) / (norm1 * norm2)))
 
 
 def get_record_lines(records: list, rec_type: str=None) -> list:
@@ -124,11 +142,17 @@ def get_record_lines(records: list, rec_type: str=None) -> list:
     """
     lines = []
     if rec_type == "hbond":
-        fmt = "{}  {}  {}~{}--{}{:5.2f} {:6.0f}\n"
+        fmt = "{:<15} {:<15} {:>16} {:5.2f}  {:3.0f}  {}  {}\n"
         for record in records:
-            donor, h, acceptor, distance, angle = record
-            line = fmt.format(donor.confID, acceptor.confID,
-                              donor.atom_name, h.atom_name, acceptor.atom_name, distance, angle)
+            donor, h, acceptor, distance, angle, xyzd, xyza = record
+            hbatms = "{:>}~{:<}...{:<4}".format(donor.atom_name.strip(),
+                                                  h.atom_name.strip(),
+                                                  acceptor.atom_name.strip())
+            line = fmt.format(donor.confID,
+                              acceptor.confID,
+                              hbatms,
+                              distance, angle,
+                              xyzd, xyza)
             lines.append(line)
     else:
         fmt ="{}  {}  {}--{}~{} {:3.0f}\n"
@@ -141,7 +165,8 @@ def get_record_lines(records: list, rec_type: str=None) -> list:
     return lines
 
 
-def detect_hbonds(pdb_file: str, include_bk: bool = False, no_empty_files: bool = False, out_dir: str = "") -> Tuple[int, int]:
+def detect_hbonds(pdb_file: str, no_bk: bool = False,
+                  no_empty_files: bool = False, out_dir: str = "") -> Tuple[int, int]:
     """
     Detect hydrogen bonds between pairs of conformers atoms in a mcce pdb file and
     save their list to file <pdb name>_hah.txt.
@@ -159,8 +184,9 @@ def detect_hbonds(pdb_file: str, include_bk: bool = False, no_empty_files: bool 
          Check D-H-A angle
          Check if angle between connected atoms on Acceptor intersect the D--H--A path
          If all passed, record a potential hydrogen bond
-    6. Write the hydrogen bond list to  <pdb name>_hah.txt
-    7. Write the blocking atoms list to  <pdb name>_blocking.txt, if found
+    6. Write the hydrogen bond list to <pdb name>_hah.txt
+    7. Write the blocking atoms list to <pdb name>_blocking.txt if found, or write an 
+       empty file if no_empty_files is False (default)
 
     Returns:
       A 2-tuple indicating whether donors acceptors and blocking atoms are found; e.g.:
@@ -199,11 +225,11 @@ def detect_hbonds(pdb_file: str, include_bk: bool = False, no_empty_files: bool 
     print("Detecting hydrogen bonds...")
     print(CRITERIA)
     print("Input file:    %s" % (pdb_file))
-    print(f"With BK: {['No','Yes'][int(include_bk)]}\n")
+    print(f"With BK: {['Yes','No'][int(no_bk)]}\n")
 
     lines = []
     # Parse the input pdb file for coordinates lines, with or without BK:
-    if not include_bk:
+    if no_bk:
         pattern = re.compile(r"^[ATOM|HETATM].{79}(?!BK).*$", re.MULTILINE) 
     else:
         pattern = re.compile(r"^[ATOM  |HETATM].*$", re.MULTILINE)
@@ -264,20 +290,25 @@ def detect_hbonds(pdb_file: str, include_bk: bool = False, no_empty_files: bool 
                         if angle > ANGCUT:  # good D--H--A angle
                             blocking = False
                             for q in acceptor.conn12:
-                                # donor and acceptor are in different conformers: no need to check if q is donor
+                                # donor and acceptor are in different conformers:
+                                # no need to check if q is donor
                                 Vqa = vec(q, acceptor)
                                 blocking_angle = deg_angle(Vqa, Vha)
-                                # connected atoms on acceptor block the D--H--A path if their angle
-                                # with the acceptor is < ANGBLOCK
+                                # connected atoms on acceptor block the D--H--A path
+                                # if their angle with the acceptor is < ANGBLOCK
                                 if blocking_angle < ANGBLOCK:
                                     blocking = True
                                     blocking_records.append((h, acceptor, q, blocking_angle))
                                     break
                             if not blocking:
                                 distance_h2a = dist(h, acceptor)
-                                hbond_records.append((donor, h, acceptor, distance_h2a, angle))
+                                hbond_records.append((donor, h, acceptor,
+                                                      distance_h2a, angle,
+                                                      donor.xyz,
+                                                      acceptor.xyz))
 
     with open(hah_fp, "w") as hah:
+        hah.write("confid_donor    confid_acceptor hb_atoms         dist  angle xyz_donor               xyz_acceptor\n")
         hah.writelines(get_record_lines(hbond_records, rec_type="hbond"))
     print(f"Output file: {hah_fp!s}")
 
@@ -290,7 +321,7 @@ def detect_hbonds(pdb_file: str, include_bk: bool = False, no_empty_files: bool 
 
     return 1,1
 
-# mcce4_tools/detect_hbonds cmd
+
 USAGE = """
   detect_hbonds [step2_out.pdb]
   detect_hbonds --out_dir <path/to/location/different/from/pdb/parent/folder>
@@ -309,10 +340,10 @@ def cli_parser():
                     type=str,
                     help="Input pdb file in mcce format. Default: %(default)s.",
                     )
-    p.add_argument("--include_bk",
+    p.add_argument("--no_bk",
                     default=False,
                     action="store_true",
-                    help="Include backbone atoms? default: %(default)s."
+                    help="Exclude backbone atoms? default: %(default)s."
                     )
     p.add_argument("--no_empty_files",
                     default=False,
@@ -342,6 +373,8 @@ def cli(argv=None):
     if block_fp.exists():
         block_fp.unlink()
 
-    result = detect_hbonds(args.inpdb, args.include_bk, args.no_empty_files, args.out_dir)
+    result = detect_hbonds(args.inpdb, args.no_bk, args.no_empty_files, args.out_dir)
     if result[0]:
-        print("hbonds collection over.")
+        print("H_bonds collection over.")
+
+    return
